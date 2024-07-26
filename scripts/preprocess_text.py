1bc2dd32e08d20d112297911752a24021bd94a34
Ahmed Hammad <32166834+bluehybrid@users.noreply.github.com>
2024-07-27 01:05:45 +0300

    Update preprocess_text.py

--- scripts/preprocess_text.py
+++ scripts/preprocess_text.py
@@ -15,7 +15,7 @@ def write_lines_to_file(path, lines, mode='w', encoding='utf-8'):
 # %%
 
 
-lines = read_lines_from_file('./data/train-orthographic-transcript.txt')
+lines = read_lines_from_file('/home/drmostafa/Research/Arabic TTS Hammad Diacritised/Tacotron 2 (PyTorch)/tts-arabic-pytorch-master/output3.txt')
 #lines = read_lines_from_file('./data/test-orthographic-transcript.txt')
 
 new_lines_arabic = []
@@ -25,21 +25,22 @@ new_lines_buckw = []
 for line in lines:
     wav_name, utterance = line.split('" "')
     wav_name, utterance = wav_name[1:], utterance[:-1]
-    utterance = utterance.replace("a~", "~a") \
-                         .replace("i~", "~i") \
-                         .replace("u~", "~u") \
-                         .replace(" - ", " ")
+   # utterance = utterance.replace("a~", "~a") \
+    #                     .replace("i~", "~i") \
+     #                    .replace("u~", "~u") \
+      #                   .replace(" - ", " ")
 
-    utterance_arab = text.buckwalter_to_arabic(utterance)
-    utterance_phon = text.buckwalter_to_phonemes(utterance)
+    utterance_buckw = text.arabic_to_buckwalter(utterance)
+    utterance_phon = text.buckwalter_to_phonemes(utterance_buckw)
 
-    line_new_ara = f'"{wav_name}" "{utterance_arab}"'
+
+    line_new_ara = f'"{wav_name}" "{utterance}"'
     new_lines_arabic.append(line_new_ara)
 
     line_new_pho = f'"{wav_name}" "{utterance_phon}"'
     new_lines_phonetic.append(line_new_pho)
 
-    line_new_buckw = f'"{wav_name}" "{utterance}"'
+    line_new_buckw = f'"{wav_name}" "{utterance_buckw}"'
     new_lines_buckw.append(line_new_buckw)
 
 
